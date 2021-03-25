c7c022b28a2351ab4dc8c4aebf17981c403b8562
Thavendren Naicker <thavendren@gmail.com>
2021-03-25 12:36:38 +0200

    Added editor config, and fixed some variable placement

--- Assets/Scripts/PID.cs
+++ Assets/Scripts/PID.cs
@@ -5,80 +5,76 @@ using System.Collections;
 [Serializable]
 public class PID
 {
-	private float _kp;
-	private float _ki;
-	private float _kd;
+    private float _p, _i, _d;
+    private float _kp, _ki, _kd;
+    private float _prevError;
 
-	/// <summary>
-	/// Constant proportion
-	/// </summary>
-	public float Kp
-	{
-        get
-        {
-			return _kp;
-        }
-        set
-        {
-			_kp = value;
-        }
-	}
-
-	/// <summary>
-	/// Constant integral
-	/// </summary>
-	public float Ki
+    /// <summary>
+    /// Constant proportion
+    /// </summary>
+    public float Kp
     {
         get
         {
-			return _ki;
+            return _kp;
         }
         set
         {
-			_ki = value;
+            _kp = value;
         }
     }
 
-	/// <summary>
-	/// Constant derivative
-	/// </summary>
-	public float Kd 
-	{
+    /// <summary>
+    /// Constant integral
+    /// </summary>
+    public float Ki
+    {
         get
         {
-			return _kd;
+            return _ki;
         }
         set
         {
-			_kd = value;
+            _ki = value;
         }
-	}
-	
-	private float _p, _i, _d;
-	private float _prevError;
-	
-	public PID(float p, float i, float d)
-    {
-		_kp = p;
-		_ki = i;
-		_kd = d;
     }
 
-	/// <summary>
-	/// Based on the code from Brian-Stone on the Unity forums
-	/// https://forum.unity.com/threads/rigidbody-lookat-torque.146625/#post-1005645
-	/// </summary>
-	/// <param name="currentError"></param>
-	/// <param name="deltaTime"></param>
-	/// <returns></returns>
-	public float GetOutput(float currentError, float deltaTime)
-	{
-		_p = currentError;
-		_i += _p * deltaTime;
-		_d = (_p - _prevError) / deltaTime;
-		_prevError = currentError;
+    /// <summary>
+    /// Constant derivative
+    /// </summary>
+    public float Kd
+    {
+        get
+        {
+            return _kd;
+        }
+        set
+        {
+            _kd = value;
+        }
+    }
 
-		return _p * Kp + _i * Ki + _d * Kd;
-	}
+    public PID(float p, float i, float d)
+    {
+        _kp = p;
+        _ki = i;
+        _kd = d;
+    }
 
+    /// <summary>
+    /// Based on the code from Brian-Stone on the Unity forums
+    /// https://forum.unity.com/threads/rigidbody-lookat-torque.146625/#post-1005645
+    /// </summary>
+    /// <param name="currentError"></param>
+    /// <param name="deltaTime"></param>
+    /// <returns></returns>
+    public float GetOutput(float currentError, float deltaTime)
+    {
+        _p = currentError;
+        _i += _p * deltaTime;
+        _d = (_p - _prevError) / deltaTime;
+        _prevError = currentError;
+        
+        return _p * Kp + _i * Ki + _d * Kd;
+    }
 }
